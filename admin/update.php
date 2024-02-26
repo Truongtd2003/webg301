ee9e2755ea3dd041759beb5886271365cd3c6370
Truongtd2003 <truongtdforword03@gmail.com>
2024-02-26 22:27:00 +0700

    first commit push set up cocde

---
+++ admin/update.php
@@ -0,0 +1,173 @@
+<?php 
+require_once('../database.php');
+
+
+$errors = [];
+
+function isFormValidated(){
+    global $errors;
+    return count($errors) == 0;
+}
+
+function checkForm(){
+    global $errors;
+    
+
+
+    $id = $_POST['admin_id'];
+    $admin = find_admin_by_id($id);
+    if (empty($_POST['username'])){
+        $errors[] = 'Admin Name is required';
+    }
+
+    if (empty($_POST['hash_password'])){
+        $errors[] = 'Password is required';
+    }
+
+    if (empty($_POST['email'])){
+        $errors[] = 'Email is required';
+    }
+    if (empty($_POST['newpassword'])){
+        $errors[] = 'New Password is required';
+    }
+    if (empty($_POST['confirmpassword'])){
+        $errors[] = 'Confirm password is required';
+    }
+    $hashed_password = hash('sha256', $_POST['hash_password']);
+    
+    if($hashed_password != $admin['hash_password']){
+        $errors[] = 'Incorrect password';
+        
+    }
+    if($_POST['confirmpassword'] != $_POST['newpassword']){
+
+        $errors[] = 'Incorrect new password';
+    }
+
+
+    
+
+}
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST'){
+    checkForm();
+    if (isFormValidated()){
+        $admin = [];
+        $admin['admin_id'] =  $_POST['admin_id'];
+        $admin['username'] =  $_POST['username'];
+        $admin['hash_password'] =  $_POST['newpassword'];
+        $admin['email'] =  $_POST['email'];
+        
+
+        update_admin($admin);
+        header('Location: index.php');
+exit;
+
+
+    }
+
+
+}
+else{
+    if(!isset($_GET['id'])) {
+        redirect_to('index.php');
+    }
+    $id = $_GET['id'];
+    $admin = find_admin_by_id($id);
+
+}
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    
+
+    </head>
+  <body>
+  <?php if ($_SERVER["REQUEST_METHOD"] == 'POST' && !isFormValidated()): ?> 
+        <div class="alert alert-danger" role="alert">
+            <span> Please fix the following errors </span>
+            <ul>
+                <?php
+                foreach ($errors as $key => $value){
+                    if (!empty($value)){
+                        echo '<li>', $value, '</li>';
+                    }
+                }
+                ?>
+            </ul>
+        </div>
+    <?php endif; ?>
+
+
+  <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post" class="container mt-4">
+        <input type="hidden" name="admin_id" 
+        value="<?php echo isFormValidated()? $admin['admin_id']: $_POST['admin_id'] ?>" >
+
+        <div class="form-group">
+            <label for="name">Name</label> 
+        <input type="text" class="form-control" id="username" name="username" value="<?php echo isFormValidated()? $admin['username']: $_POST['username'] ?>">
+        </div>
+            <div class="form-group">
+                <label for="email">Email: </label>
+                <input type="text" class="form-control" id="email" name="email" value="<?php echo isFormValidated()? $admin['email']: $_POST['email'] ?>">
+            </div>
+            <div class="form-group">
+            <label for="hash_password">PassWord</label> <!--required-->
+        <input type="text" class="form-control" id="hash_password" name="hash_password" value="<?php echo isFormValidated()? '': $_POST['hash_password'] ?>">
+            </div>
+
+            <div class="form-group">
+            <label for="newpassword">New PassWord</label> <!--required-->
+        <input type="text" class="form-control" id="newpassword" name="newpassword" value="<?php echo isFormValidated()? '': $_POST['newpassword'] ?>">
+            </div>
+
+            <div class="form-group">
+            <label for="confirmpassword">Confirm PassWord</label> <!--required-->
+        <input type="text" class="form-control" id="confirmpassword" name="confirmpassword" value="<?php echo isFormValidated()? '': $_POST['confirmpassword'] ?>">
+            </div>
+           
+        
+        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+        
+    </form>
+
+    <div class="container mt-4">
+        <a href="index.php" class="btn btn-info">Back to index</a> 
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
+
+<?php
+db_disconnect($db);
+?>
---
+++ database.php
@@ -0,0 +1,79 @@
+<?php
+
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "handicraft");
+
+
+function db_connect(){
+    $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+    return $connection;
+}
+
+$db = db_connect();
+
+function db_disconnect($connection) { //call at the end of each page
+    if(isset($connection)) {
+      mysqli_close($connection);
+    }return $connection;
+}
+
+
+function confirm_query_result($result){
+    global $db;
+    if (!$result){
+        echo mysqli_error($db);
+        db_disconnect($db);
+        exit; 
+    } else {
+        return $result;
+    }
+  }
+
+
+
+  function find_all_admin(){
+    global $db;
+
+    $sql = "SELECT * FROM admins ";
+    $sql .= "ORDER BY username";
+    //echo $sql;
+    $result = mysqli_query($db, $sql); 
+    // For SELECT statements, $result is a set of data
+    return $result; 
+}
+
+
+function update_admin($admin) {
+    global $db;
+    $hashed_password = hash('sha256', $admin['hash_password']);
+    
+  
+    $sql = "UPDATE admins SET ";
+    $sql .= "username='" . $admin['username'] . "', ";
+    $sql .= "hash_password='" . $hashed_password . "', ";
+    $sql .= "email='" . $admin['email'] . "' ";
+    $sql .= "WHERE admin_id='" . $admin['admin_id'] . "' ";
+    $sql .= "LIMIT 1";
+    $result = mysqli_query($db, $sql);
+    return confirm_query_result($result);
+  }
+
+  function find_admin_by_id($id) {
+    global $db;
+  
+    $sql = "SELECT * FROM admins ";
+    $sql .= "WHERE admin_id='" . $id . "'";
+    $result = mysqli_query($db, $sql);
+    confirm_query_result($result);
+    $admin = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+    return $admin; // returns an assoc. array
+  }
+
+
+  function redirect_to($location) {
+    header("Location: " . $location);
+    exit;
+  }
